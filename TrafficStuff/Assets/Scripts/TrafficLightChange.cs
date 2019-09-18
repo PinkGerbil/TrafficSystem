373c3495b568168a497c084af71dd129a0e98eb7
PinkGerbil <thecavingtroll@gmail.com>
2019-09-19 09:47:54 +1000

    no message

---
+++ TrafficStuff/Assets/Scripts/TrafficLightChange.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrafficLightChange : MonoBehaviour
+{
+    Material Light_Red;
+    Material Light_Blue;
+    Material Light_Green;
+
+    float RedTimer;
+    float YellowTimer;
+    float GreenTimer;
+
+    float RedTimerLimit;
+    float YellowTimerLimit;
+    float GreenTimerLimit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RedTimer += Time.deltaTime;
+        YellowTimer += Time.deltaTime;
+        GreenTimer += Time.deltaTime;
+
+
+    }
+
+    private void LightChangeToRed()
+    {
+
+    }
+
+    private void LightChangeToYellow()
+    {
+
+    }
+
+    private void LightChangeToGreen()
+    {
+
+    }
+}
