c0d9d264020ef798e55e6fa917e3d24c415196fe
PinkGerbil <thecavingtroll@gmail.com>
2019-09-19 11:05:17 +1000

    no message

--- TrafficStuff/Assets/Scripts/TrafficLightChange.cs
+++ TrafficStuff/Assets/Scripts/TrafficLightChange.cs
@@ -4,17 +4,21 @@ using UnityEngine;
 
 public class TrafficLightChange : MonoBehaviour
 {
-    Material Light_Red;
-    Material Light_Blue;
-    Material Light_Green;
+    public Material Light_Red;
+    public Material Light_Yellow;
+    public Material Light_Green;
 
-    float RedTimer;
-    float YellowTimer;
-    float GreenTimer;
+    public float RedTimer;
+    public float YellowTimer;
+    public float GreenTimer;
 
-    float RedTimerLimit;
-    float YellowTimerLimit;
-    float GreenTimerLimit;
+    public float RedTimerLimit;
+    public float YellowTimerLimit;
+    public float GreenTimerLimit;
+
+    public bool RedLightActive = true;
+    public bool YellowLightActive = false;
+    public bool GreenLightActive = false;
 
     // Start is called before the first frame update
     void Start()
@@ -29,21 +33,62 @@ public class TrafficLightChange : MonoBehaviour
         YellowTimer += Time.deltaTime;
         GreenTimer += Time.deltaTime;
 
+        //if (Input.GetKeyDown(KeyCode.Alpha1))
+        //{
+        //    LightChangeToRed();
+        //}
+        //if (Input.GetKeyDown(KeyCode.Alpha2))
+        //{
+        //    LightChangeToYellow();
+        //}
+        //if (Input.GetKeyDown(KeyCode.Alpha3))
+        //{
+        //    LightChangeToGreen();
+        //}
 
+
+        if(GreenTimer > GreenTimerLimit && GreenLightActive)
+        {
+            GreenLightActive = false;
+            LightChangeToYellow();
+        }
+        if(YellowTimer > YellowTimerLimit && YellowLightActive)
+        {
+            YellowLightActive = false; 
+            LightChangeToRed();
+        }
+        if(RedTimer > RedTimerLimit && RedLightActive)
+        {
+            RedLightActive = false;
+            LightChangeToGreen();
+        }
     }
 
     private void LightChangeToRed()
     {
-
+        RedLightActive = true;
+        ResetTimers();
+        this.gameObject.GetComponent<MeshRenderer>().material = Light_Red;
     }
 
     private void LightChangeToYellow()
     {
-
+        YellowLightActive = true;
+        ResetTimers();
+        this.gameObject.GetComponent<MeshRenderer>().material = Light_Yellow;
     }
 
     private void LightChangeToGreen()
     {
+        GreenLightActive = true;
+        ResetTimers();
+        this.gameObject.GetComponent<MeshRenderer>().material = Light_Green;
+    }
 
+    private void ResetTimers()
+    {
+        RedTimer = 0;
+        YellowTimer = 0;
+        GreenTimer = 0; 
     }
 }
